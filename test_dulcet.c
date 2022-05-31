d452b27400672f3fd495ff3df3983d80bfd1a027
Leonardo Duarte <weltschildkroete@gmail.com>
2022-05-31 09:45:31 -0300

    Use VAR, ABS, APP macros in test_dulcet.c to aid readability
    Akin to what is already done in test_dulcet_parser.c

--- test_dulcet.c
+++ test_dulcet.c
@@ -9,20 +9,21 @@
 
 #include "dulcet.h"
 
+#define VAR(x) dulcet_alloc_var(x)
+#define ABS(m) dulcet_alloc_abs(m)
+#define APP(m, n) dulcet_alloc_app(m, n)
+
 ZIDANE_TEST(sanity)
 {
-	struct dulcet_term *x =
-		dulcet_alloc_app(dulcet_alloc_abs(dulcet_alloc_var(1)), dulcet_alloc_var(2));
+	struct dulcet_term *x = APP(ABS(VAR(1)), VAR(2));
 
 	dulcet_term_free(x);
 }
 
 ZIDANE_TEST(term_eq)
 {
-	struct dulcet_term *x =
-		dulcet_alloc_app(dulcet_alloc_abs(dulcet_alloc_var(1)), dulcet_alloc_var(2));
-	struct dulcet_term *y =
-		dulcet_alloc_app(dulcet_alloc_abs(dulcet_alloc_var(1)), dulcet_alloc_var(2));
+	struct dulcet_term *x = APP(ABS(VAR(1)), VAR(2));
+	struct dulcet_term *y = APP(ABS(VAR(1)), VAR(2));
 
 	ZIDANE_VERIFY(dulcet_term_eq(x, y));
 
@@ -32,26 +33,15 @@ ZIDANE_TEST(term_eq)
 
 ZIDANE_TEST(beta_nor_pred_succ)
 {
-	struct dulcet_term *succ =
-		dulcet_alloc_abs(dulcet_alloc_abs(dulcet_alloc_abs(dulcet_alloc_app(
-			dulcet_alloc_var(2),
-			dulcet_alloc_app(dulcet_alloc_app(dulcet_alloc_var(3), dulcet_alloc_var(2)),
-					 dulcet_alloc_var(1))))));
-	struct dulcet_term *pred =
-		dulcet_alloc_abs(dulcet_alloc_abs(dulcet_alloc_abs(dulcet_alloc_app(
-			dulcet_alloc_app(
-				dulcet_alloc_app(dulcet_alloc_var(3),
-						 dulcet_alloc_abs(dulcet_alloc_abs(dulcet_alloc_app(
-							 dulcet_alloc_var(1),
-							 dulcet_alloc_app(dulcet_alloc_var(2),
-									  dulcet_alloc_var(4)))))),
-				dulcet_alloc_abs(dulcet_alloc_var(2))),
-			dulcet_alloc_abs(dulcet_alloc_var(1))))));
+	struct dulcet_term *succ = ABS(ABS(ABS(APP(VAR(2), APP(APP(VAR(3), VAR(2)), VAR(1))))));
+	struct dulcet_term *pred = ABS(ABS(
+		ABS(APP(APP(APP(VAR(3), ABS(ABS(APP(VAR(1), APP(VAR(2), VAR(4)))))), ABS(VAR(2))),
+			ABS(VAR(1))))));
 
-	struct dulcet_term *actual = dulcet_alloc_app(pred, succ);
+	struct dulcet_term *actual = APP(pred, succ);
 	dulcet_beta_nor(actual);
 
-	struct dulcet_term *expected = dulcet_alloc_abs(dulcet_alloc_abs(dulcet_alloc_var(1)));
+	struct dulcet_term *expected = ABS(ABS(VAR(1)));
 
 	ZIDANE_VERIFY(dulcet_term_eq(actual, expected));
 
